c28fd4261fcd047ab39899ddc079bcc6e1d5925d
airplane <1812921361@qq.com>
2022-12-27 17:40:11 +0800

    default

--- src/main/java/io/FOSDemo.java
+++ src/main/java/io/FOSDemo.java
@@ -1,5 +1,22 @@
 package io;
 
-public class FOSDemo {
+import java.io.FileOutputStream;
 
+/**
+ * 文件的输入输出
+ */
+public class FOSDemo {
+    public static void main(String[] args) throws Exception {
+        /*
+        文件流输出流在创建时，
+        若指定路径下文件不存在时，系统会直接创建文件
+        但是文件目录必须存在
+        */
+        FileOutputStream fos=new FileOutputStream("./fos.dat");
+        for (int i = 0; i < 26; i++) {
+            fos.write(65+i);
+        }
+        System.out.println("写入完毕");
+        fos.close();
+    }
 }
